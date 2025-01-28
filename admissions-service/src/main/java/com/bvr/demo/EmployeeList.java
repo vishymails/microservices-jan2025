ed31bfcd234b91346c3a48754e7b49b9118d2322
vishymails <vishymails@gmail.com>
2025-01-28 13:00:04 +0530

    added

---
+++ admissions-service/src/main/java/com/bvr/demo/EmployeeList.java
@@ -0,0 +1,23 @@
+package com.bvr.demo;
+
+import java.util.List;
+
+public class EmployeeList {
+
+	public EmployeeList() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	private List<Employee> employee;
+
+	public List<Employee> getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(List<Employee> employee) {
+		this.employee = employee;
+	}
+	
+	
+
+}
--- pathology-service/src/main/java/com/bvr/demo/PathologyResource.java
+++ pathology-service/src/main/java/com/bvr/demo/PathologyResource.java
@@ -1,11 +1,43 @@
 package com.bvr.demo;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pathology")
 public class PathologyResource {
 
 	public PathologyResource() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	List<Disease> diseases = Arrays.asList(
+			new Disease("d1", "Dengue", "Be safe from still water"),
+			new Disease("d2", "Headache", "hot water vapour"),
+			new Disease("d3", "Corona", "wash hands and stay safe")
+			
+			);
+	
+	@RequestMapping("/diseases")
+	public DiseaseList diseases() {
+		
+		DiseaseList diseaseList = new DiseaseList();
+		diseaseList.setDiseases(diseases);
+		return diseaseList;
+	}
+	
+	
+	@RequestMapping("/diseases/{id}")
+	public Disease getDiseaseById(@PathVariable("id") String id) {
+		Disease d = diseases.stream()
+					.filter(disease -> id.equals(disease.getId()))
+					.findAny()
+					.orElse(null);
+		return d;
+	}
 
-	
-	
 }
